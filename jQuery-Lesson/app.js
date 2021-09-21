714dd78c0736b46c829a80ec5cd998007eef0c74
Hiroki1112Kurata <sequoia1112@yahoo.co.jp>
2021-09-21 17:24:19 +0900

    [Add]mouseover

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -8,15 +8,33 @@
 //   });
 // });
 
-js-4章 スライドUP・DOWN
+// js-4章 スライドUP・DOWN
 // cssにて、 display: none;をONにする
-$(function () {
-  $('.box1').slideDown(1500, function () {
-    $('.box1').css({
-      'background-color': '#0000FF',
-      'width': '200px',
-      'height': '100px'
-    }).slideUp(2000);
+  // $(function () {
+  //   $('.box1').slideDown(1500, function () {
+  //     $('.box1').css({
+  //       'background-color': '#0000FF',
+  //       'width': '200px',
+  //       'height': '100px'
+  //     }).slideUp(2000);
+  //   });
+  // });
+
+
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').css({'background-color': '#0000FF'});
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').css({'background-color': '#FF0000'});
+//   });
+// });
+
+$(function(){
+  $('.box1').mouseover(function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
   });
 });
-
